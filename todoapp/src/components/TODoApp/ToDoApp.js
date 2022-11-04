07ac50248834e40078d8a04a34aabae3ca7a5b14
Athif binu <binuathif@gmail.com>
2022-11-04 20:41:50 +0530

    finsih

--- todoapp/src/components/TODoApp/ToDoApp.js
+++ todoapp/src/components/TODoApp/ToDoApp.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react'
 import "./ToDoApp.css";
+
+
 export default class ToDoApp extends Component {
+  state={
+    input:"",
+    items:[]
+  }
+
+  handlechange=event=>{
+    this.setState({
+      input:event.target.value
+    })
+    console.log(this.state.input)
+  };
+  storeItems=event=>{
+    event.preventDefault();    //used to avoid refresh page
+    const {input}=this.state;
+   
+
+    this.setState({
+        items:[...this.state.items,input],
+        input:""  //remove next step in adding
+
+
+    });
+
+  };
+
+  deleteItem=key=>{
+     const allitems=this.state.items;
+
+     allitems.splice(key,1);
+
+     this.setState({
+      items:allitems
+     })
+  }
   render() {
+    const {input,items}=this.state
+    console.log(items)
     return (
       <div className='todo-container'>
+        <form className='input-section' onSubmit={this.storeItems}>
         <h1>ToDo App</h1>
-
-        <form className='input-section'>
-          <input type="text" placeholder='Enter Items...'/>
+          <input type="text" value={input} onChange={this.handlechange} placeholder='Enter Items...'/>
 
         </form>
 
         <ul>
-          <li>Items</li>
+          {items.map((data,index)=> (
+              <li key={index}>{data} <i class="fa-sharp fa-solid fa-trash" onClick={()=>this.deleteItem(index)}></i></li>
+          ))}
         </ul>
         
       </div>
     )
   }
 }
+
+// click index=clickbtn index same=> deleat
